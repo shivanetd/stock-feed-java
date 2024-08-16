8888e37a9d89b3af58d71ad1996d2f0b3cc30da6
shivanetd <shiva.netd@gmail.com>
2024-08-16 11:55:50 +0000

    Add Json types for jackson to choose right model for parsing

--- src/main/java/com/shiva/stockfeed/model/FeedableEntity.java
+++ src/main/java/com/shiva/stockfeed/model/FeedableEntity.java
@@ -1,7 +1,16 @@
 package com.shiva.stockfeed.model;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.shiva.stockfeed.handler.FeedHandler;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "T")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = StockBarMessage.class, name = "b"),
+    @JsonSubTypes.Type(value = StockTradeMessage.class, name = "t"),
+    @JsonSubTypes.Type(value = StockQuoteMessage.class, name = "q"),
+    @JsonSubTypes.Type(value = BaseStockMessage.class, name = "success")
+})
 public interface FeedableEntity {
     String getType();
     
